70edcf4cacbcef0a759b35549c8208ae153c1bfe
MGerberick <197294373+MGerberick@users.noreply.github.com>
2025-07-28 12:39:26 -0400

    Exercise 07-reverseString Complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const newStr = str.split('');
+    newStr.reverse();
+    return newStr.join('');
+    //return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
