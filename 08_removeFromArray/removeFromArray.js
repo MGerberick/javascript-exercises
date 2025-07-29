77a2fe39a7c5c093698a03dbd38f4b24475f9db2
MGerberick <197294373+MGerberick@users.noreply.github.com>
2025-07-29 09:22:11 -0400

    Exercise 08 - removeFromArray Complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+   //   args.forEach(arg => {
+   //      for (val of arr) {
+   //       // console.log(val, arr, arg);
+   //       let inx = arr.findIndex(item => (item === arg));
+   //       // console.log(inx);
+   //       if (inx >= 0) {arr.splice(inx,1)};
+   //       // console.log(arr);
+   //      }
+   //   })
+   //   return arr;
+   return arr.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
