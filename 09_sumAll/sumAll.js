b47780f8b5229a51507ca56f751cb66ea26b4f55
MGerberick <197294373+MGerberick@users.noreply.github.com>
2025-07-29 11:26:50 -0400

    09_sumAll Exercises Complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // console.log(num1, num2);
+    switch(true) {
+        case (num1 < 0 || num2 < 0):
+        case (Math.floor(num1) != num1 || Math.floor(num2) != num2):
+        case (typeof num1 != "number" || typeof num2 != "number"):
+         return "ERROR";
+    };
+    let newNum = 0;
+    let a=0;
+    let b=0;
+    if(num1>num2){a=num2; b=num1} else {a=num1; b=num2}
+    for (i = a; i <= b; i++) {
+        newNum += i;
+    };
+    return newNum;
 
 };
 
